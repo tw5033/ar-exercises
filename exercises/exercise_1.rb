e6ed1eaad35f02ffa91433c638a86dc5a4129230
tw5033 <tw5033@gmail.com>
2019-05-14 18:21:52 +0000

    Finish exercise 2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -7,3 +7,5 @@ puts "----------"
 store1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 store2 = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 store3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts Store.count
