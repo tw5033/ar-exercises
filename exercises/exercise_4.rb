2d53edec0cdf146ef0eb5475762166f017c40dd4
tw5033 <tw5033@gmail.com>
2019-05-14 18:41:59 +0000

    Finish exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,16 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@men_stores = Store.where(mens_apparel: true)
+@men_stores.each do |store|
+  puts "#{store.name} generates $#{store.annual_revenue} per year!"
+end
+@womens_stores = Store.where(womens_apparel: true).where("annual_revenue < 1000000")
+@womens_stores.each do |store|
+  puts "#{store.name} generates $#{store.annual_revenue} per year!"
+end  
+
